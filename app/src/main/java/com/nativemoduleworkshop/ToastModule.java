2ce3f9588aae78e018af94b4a419ff2fd82bc4ff
Kenny <mccormick84@gmail.com>
2022-02-14 22:27:03 +0900

    안드로이드에서 Toast 사용하기 1)모듈 작성하기

---
+++ app/src/main/java/com/nativemoduleworkshop/ToastModule.java
@@ -0,0 +1,16 @@
+package com.nativemoduleworkshop;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class ToastModule extends ReactContextBaseJavaModule{
+    ToastModule(ReactApplicationContext context) {
+        super(context);
+    }
+
+    @Override
+    public String getName() {
+        return "ToastModule";
+    }
+}
